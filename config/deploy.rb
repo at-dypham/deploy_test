2372ce7217bccf3c3928f6f1a7b8be823f80ae48
at-dypd <dy.pham@asiantech.vn>
2017-07-28 18:03:49 +0700

    [Add] Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,10 @@ set :deploy_to, '/root/dypham'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
